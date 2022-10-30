e436494bef09eddda3536b2a78b9843be90c8eb6
torstbje <torstbje@uio.no@users.noreply.github.com>
2022-10-30 16:26:31 +0100

    Made functions for gradient descent with and without momentum

--- src/gradient_descent.py
+++ src/gradient_descent.py
@@ -24,12 +24,12 @@ def simple_descent(x,y,condition,iterations,step_size):
     beta = rng.standard_normal((2,1))
     iter = 0
 
-    while (iter < iterations):
+    for iter in range(iterations):
         gradient = find_gradient(X,y,beta)
         beta -= step_size*gradient
         iter += 1
         if abs(np.mean(gradient)) < condition:
-            print('Iterations without momentum: ',iter)
+            print('Iterations GD: ',iter)
             break
 
     ypredict = X @ beta
@@ -47,14 +47,72 @@ def momentum_descent(x,y,condition,iterations, step_size, momentum):
     iter = 0
     change = 0
 
-    while (iter < iterations):
+    for iter in range(iterations):
         gradient = find_gradient(X, y, beta)
         change = step_size * gradient + momentum * change
         beta = beta - change
         iter += 1
         if abs(np.mean(gradient)) < condition:
-            print('Iterations with momentum: ',iter)
+            print('Iterations GD (momentum): ',iter)
             break
 
+    ypredict = X @ beta
+    return ypredict
+
+
+def sgd(x, y, condition,iterations, step_size, M):
+    """ M: size of batch """
+    rng = default_rng()
+    n = len(y)
+    m = int(n/M) #number of minibatches
+    X = np.c_[np.ones(n), x]
+    beta = rng.standard_normal((2,1))
+    indices = rng.permuted(np.arange(0,100,1)).reshape((m,M))
+    for iter in range(iterations):
+        gradient = np.zeros((2,1))
+        for i in range(m):
+            #Pick the k-th minibatch at random
+            k = rng.integers(0,m)
+            batch_indices = indices[k]
+            X_b = X[batch_indices]
+            y_b = y[batch_indices]
+            g_b = find_gradient(X_b,y_b,beta)
+            gradient += step_size*g_b
+
+        beta -= step_size*gradient
+        if abs(np.mean(gradient)) < condition:
+            print('Iterations SDG: ', iter)
+            break
+    
+    ypredict = X @ beta
+    return ypredict
+
+
+def sgd_mom(x, y, condition,iterations, step_size, M, momentum):
+    """ M: size of batch """
+    rng = default_rng()
+    n = len(y)
+    m = int(n/M) #number of minibatches
+    X = np.c_[np.ones(n), x]
+    beta = rng.standard_normal((2,1))
+    indices = rng.permuted(np.arange(0,100,1)).reshape((m,M))
+    change = 0
+    for iter in range(iterations):
+        gradient = np.zeros((2,1))
+        for i in range(m):
+            #Pick the k-th minibatch at random
+            k = rng.integers(0,m)
+            batch_indices = indices[k]
+            X_b = X[batch_indices]
+            y_b = y[batch_indices]
+            g_b = find_gradient(X_b,y_b,beta)
+            gradient += step_size*g_b
+
+        change = step_size * gradient + momentum * change
+        beta -= change
+        if abs(np.mean(gradient)) < condition:
+            print('Iterations SDG (momentum): ', iter)
+            break
+    
     ypredict = X @ beta
     return ypredict
--- src/main.py
+++ src/main.py
@@ -1,38 +1,51 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from generate import gen_simple
-from gradient_descent import simple_descent, lin_reg, momentum_descent
+from gradient_descent import *
 
 # the number of datapoints
 n_datapoints = 100
-n_iterations = 10000
+n_iterations = 1000000
 step_size = 0.1
 momentum = 0.3
-cond = 1e-10
+cond = 1e-4
+batch_size = 5
 x, y = gen_simple(n_datapoints)
 
-ypredict_lin = lin_reg(x,y)
+
 ypredict = simple_descent(x,y, cond,n_iterations,step_size)
 ypredict_mom = momentum_descent(x, y, cond, n_iterations, step_size, momentum)
+ypredict_sdg = sgd(x, y, cond, n_iterations, step_size, batch_size)
+ypredict_sdg_mom = sgd_mom(x, y, cond, n_iterations, step_size, batch_size, momentum)
 
-plt.subplot(311)
+plt.subplot(221)
+plt.title(r'Stochastic gradient descent')
+plt.plot(x, ypredict_sdg, '-b')
+plt.plot(x, y ,'r.')
+plt.ylabel('Without momentum')
+
+
+plt.subplot(222)
 plt.title(r'Gradient descent')
-plt.plot(x, ypredict_mom, "b-", label = 'Gradient descent with momentum')
+plt.plot(x, ypredict_mom, '-b')
 plt.plot(x, y ,'r.')
-plt.ylabel(r'$y$')
-plt.legend()
 
-plt.subplot(312)
-plt.plot(x, ypredict, "b-", label = 'Gradient descent')
+plt.subplot(223)
+plt.plot(x, ypredict_sdg_mom, '-b')
 plt.plot(x, y ,'r.')
-plt.ylabel(r'$y$')
-plt.legend()
+plt.ylabel('With momentum')
 
-plt.subplot(313)
-plt.plot(x, ypredict_lin, "b-", label = 'Linear regression')
+plt.subplot(224)
+plt.plot(x, ypredict, '-b')
+plt.plot(x, y ,'r.')
+
+
+'''
+ypredict_lin = lin_reg(x,y)
+plt.plot(x, ypredict_lin, '-b')
 plt.plot(x, y ,'r.')
 plt.xlabel(r'$x$')
 plt.ylabel(r'$y$')
 plt.legend()
-
+'''
 plt.show()
