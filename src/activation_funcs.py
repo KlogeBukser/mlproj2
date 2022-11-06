7bb87cd38947c2ca8b24778ed439f19a7e1e8cfb
KlogeBukser <imvacant0923@gmail.com>
2022-11-06 17:20:30 +0100

    updated data structure

--- src/activation_funcs.py
+++ src/activation_funcs.py
@@ -1,18 +1,41 @@
 # activation_funcs.py
+from jax import grad,vjp
+import jax.numpy as np
 
 # Sigmoid, the RELU and the Leaky RELU
+
+# jax.grad requires function to have float output, hence the 0.0's, allow_int=True doesn't work
+
+class ActivationFunction:
+	"""Activation functions and their gradients"""
+	def __init__(self, func, gradient=None):
+		self.func = func
+
+		if gradient:
+			self.gradient = gradient
+		else:
+			self.auto_grad_it()
+
+	def auto_grad():
+		self.gradient = grad(self.func)
 
 def sigmoid(x):
 	return 1/(1+np.exp(-x))
 
+def relu(x):
+	if x > 0:
+		return x
+	return 0.0
 
-def relu():
-	pass
-
-def leaky_relu():
-	pass
+def leaky_relu(x, alpha):
+	if x >= 0:
+		return x 
+	return alpha*x
 
 def heaviside(x):
 	if x > 0:
-		return 1
-	return 0
+		return 1.0
+	return 0.0
+
+
+
--- src/nn.py
+++ src/nn.py
@@ -1,32 +1,35 @@
 from jax import numpy as jnp
 import numpy as np
+from activation_funcs import *
+from gradient_descent import *
 
-# print(np.random.randn(2,3))
-class Node:
-	"""docstring for Node
-	weights: array-like, length equal to the number of nodes in the previous layer
-	biases: float, """
-	def __init__(self, n_node_in_next_layer):
-		super(Node, self).__init__()
-
-		# Do NOT initialise to 0
-		self.weights = np.ones(n) # small, (0,1), normally distribution 
-		self.bias = np.random.randn(1) # small, (0,1), normally distribution
-
-
-		
+# print(np.random.randn(2,3))	
 
 class NeuralNetwork:
-	"""docstring for NeuralNetwork
+	"""NeuralNetwork
 	X_inputs, Y_inputs, n_layers, n_nodes, n_epochs, batch_size, 
+	n_hidden_layers: int, number of HIDDEN layers
+	n_nodes: array, node count for each layer, length must equal to n_hidden_layers
+	n_catagotires: int, number of possible output, 0 for regression problems, 
 	eta, lmbd: regularisation params
 	"""
-	def __init__(self, X_inputs, Y_inputs, n_catagories, n_layers, n_nodes, n_epochs=10, batch_size=100, eta=0.01, lmbd=0.0):
+	def __init__(
+			self, 
+			X_inputs, 
+			Y_inputs,  
+			n_hidden_layers, 
+			n_nodes, 
+			gd_func,
+			n_catagories=0, # only for classification
+			n_epochs=10, 
+			batch_size=100, 
+			eta=0.01, 
+			lmbd=0.0):
 		super(NeuralNetwork, self).__init__()
 
 		self.X_inputs = X_inputs
 		self.Y_inputs = Y_inputs
-		self.n_layers = n_layers
+		self.n_hidden_layers = n_hidden_layers
 		self.n_nodes = n_nodes
 
 		self.n_inputs = X_inputs.shape[0]
@@ -38,20 +41,24 @@ class NeuralNetwork:
 		self.n_iter = self.n_inputs // self.batch_size
 
 
+		self.eta = eta
+		self.lmbd = lmbd
+
+		self.gradient_descent = gd_func
+		self.acti_func = acti_func
+		self.acti_func_out = sigmoid
+
+		self.create_biases_and_weights()
+
+
 	# structural methods below
 
 	def create_biases_and_weights(self):
-		pass
+		'''Initialise weights and biases vectors/matrices/tensors'''
+		self.weightss = np.zeros(n_hidden_layers)
+		for i in range(self.n_hidden_layers+1):
+			continue
 
-	def add_node(self, node, ind_layer, ind_node):
-		pass
-
-	def cost_func(self):
-		pass
-
-
-	# def gradient_desent(self):
-	# 	pass
 
 
 	# def regularisation(self):
@@ -64,6 +71,7 @@ class NeuralNetwork:
 		pass
 
 	def feed_forward_out(self):
+		'''outputs the probabilities for each catagories'''
 		pass
 
 
@@ -78,16 +86,30 @@ class NeuralNetwork:
 		pass
 
 
+	def acti_func(self):
+		acti_func = sigmoid
+
+
 	# Evaluation
 
 	def score(self, X_test, Y_test):
-	'''Evaluation of model'''
+		'''Evaluation of model'''
 		Y_pred = self.predict(X_test)
 		return np.sum(Y_pred == Y_test) / len(Y_test)
+		
+
+class NNRegressor(NeuralNetwork):
+	"""docstring for RegressionNeuralNetwork"""
+	def __init__(self):
+		super(NNRegressor, self).__init__()
+		
 
 
-
-
+class NNClassifier(NeuralNetwork):
+	"""docstring for Classifier"""
+	def __init__(self):
+		super(NNClassifier, self).__init__()
+		
 	
 
 
