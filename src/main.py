5443b9575590e3ee97b4237a95c02fb128035c15
torstbje <torstbje@uio.no@users.noreply.github.com>
2022-11-08 19:03:18 +0100

    Made seaborn gridplot with several scatterplots to evaluate mse for different parameters

--- src/main.py
+++ src/main.py
@@ -9,4 +9,23 @@ x, y = gen_simple(n_datapoints)
 sgd_figures(x,y,eta_method = 'basic', n_features = 3,n_iterations = 100)
 
 
+n_predictions = 200
+
+df = make_dataframe_sgd(x, y, eta_method = 'basic', n_features = 3,n_iterations = 100, n_predictions = n_predictions)
+
+make_sgd_pairplot(df,method = 'basic')
+
+df = make_dataframe_sgd(x, y, eta_method = 'ada', n_features = 3,n_iterations = 100, n_predictions = n_predictions)
+
+make_sgd_pairplot(df,method = 'ada')
+
+df = make_dataframe_sgd(x, y, eta_method = 'rms', n_features = 3,n_iterations = 100, n_predictions = n_predictions)
+
+make_sgd_pairplot(df,method = 'rms')
+
+df = make_dataframe_sgd(x, y, eta_method = 'adam', n_features = 3,n_iterations = 100, n_predictions = n_predictions)
+
+make_sgd_pairplot(df,method = 'adam')
+
+
 
