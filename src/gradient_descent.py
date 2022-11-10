241daa2d80e42be7eaa5beefb409777f5af836eb
torstbje <torstbje@uio.no@users.noreply.github.com>
2022-11-10 18:42:57 +0100

    gradient_descent method will now return the average of the last two thetas. RMS and ADAM are now consistently better than the basic algo. The epoch plot uses sgd_one_epoch directly

--- src/gradient_descent.py
+++ src/gradient_descent.py
@@ -16,13 +16,13 @@ def sgd_one_epoch(X,y,rng,theta,n_batches,lmbda,eta,indices):
         batch_indices = indices[k]
         X_b = X[batch_indices]
         y_b = y[batch_indices]
-        g_b = find_gradient_ridge(X_b,y_b,theta,lmbda)
+        g_b = find_gradient(X_b,y_b,theta,lmbda)
         gradient += g_b
 
     return eta.update(gradient/n_batches)
 
 
-def gradient_descent(X, y, theta, lmbda, n_epochs, eta, n_batches):
+def gradient_descent(X, y, new_theta, lmbda, n_epochs, eta, n_batches):
     """ Gradient descent """
 
     rng = default_rng()
@@ -30,13 +30,12 @@ def gradient_descent(X, y, theta, lmbda, n_epochs, eta, n_batches):
     batch_size = int(n_datapoints/n_batches)
      
     indices = np.arange(0,n_batches*batch_size,1).reshape((n_batches,batch_size))
-    thetas = np.zeros((n_epochs,theta.shape[0]))
     for i in range(n_epochs):
+        theta = np.copy(new_theta)
         change = sgd_one_epoch(X,y,rng,theta,n_batches,lmbda,eta,indices)
-        theta += change
-        thetas[i] = theta.ravel()
+        new_theta = theta + change
     eta.reset()
-    return thetas
+    return 0.5*(theta + new_theta)
 
 def lin_reg(X,y):
     """ linear regression """
@@ -44,7 +43,7 @@ def lin_reg(X,y):
     theta = np.linalg.inv(X.T @ X) @ X.T @ y
     return theta
 
-def find_gradient_ridge(X,y,theta,lamb):
+def find_gradient(X,y,theta,lamb):
     """ Function for finding the gradient """
     n = len(y)
     return 2*((1/n)*X.T @ (X @ theta-y) + lamb*theta)
--- src/make_figure.py
+++ src/make_figure.py
@@ -35,29 +35,36 @@ def make_dataframe_sgd(x, y, n_features = 3,n_iterations = 100,n_predictions = 1
 
         eta = make_adaptive_learner(eta_method,n_features,rate)
         theta0 = np.copy(theta_init)
-        theta_final = np.mean(gradient_descent(X_train, y_train, theta0, lmbda, n_iterations, eta, batch)[-2:],axis = 0)
-        y_pred = X_test @ theta_final
+        theta = gradient_descent(X_train, y_train, theta0, lmbda, n_iterations, eta, batch)
+        y_pred = X_test @ theta
 
         df.loc[len(df.index)] = [rate,batch,np.log10(lmbda),np.log(MSE(y_pred,y_test)),eta_method]
 
     return df
 
 
-def make_epoch_plot(x, y, n_iterations,n_features,lmbda,eta_method,n_batches,learning_rate):
+def make_epoch_plot(x, y, n_epochs,n_features,lmbda,eta_method,n_batches,learning_rate):
 
-    iterations = np.arange(0,n_iterations,1)
+    iterations = np.arange(0,n_epochs,1)
     X = make_design_1D(x,n_features)
     X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
 
     eta = make_adaptive_learner(eta_method,n_features,learning_rate)
 
     
-    theta_init = guess_initial_theta(X_train[1],y_train,n_features)
-    thetas = gradient_descent(X_train, y_train, theta_init, lmbda, n_iterations, eta, n_batches)
-    mses = np.empty(thetas.shape[0])
-    for i in iterations:
-        y_pred = X_test @ thetas[i]
-        mses[i] = MSE(y_test,y_pred)
+    rng = default_rng()
+    n_datapoints = len(y_train)
+    batch_size = int(n_datapoints/n_batches)
+     
+    indices = np.arange(0,n_batches*batch_size,1).reshape((n_batches,batch_size))
+    theta = guess_initial_theta(X_train[1],y_train,n_features)
+    mses = np.empty(n_epochs)
+    for i in range(n_epochs):
+        change = sgd_one_epoch(X_train,y_train,rng,theta,n_batches,lmbda,eta,indices)
+        theta += change
+        mses[i] = MSE(X_test @ theta, y_test)
+    eta.reset()
+
 
     plt.title("MSE over iterations with " + eta_method + " algorithm. (" + str(n_batches) + " minibatches)")
     plt.xlabel("Epochs")
