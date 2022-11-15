00227285981a7483cdea62317f8bb17f096c6732
torstbje <torstbje@uio.no@users.noreply.github.com>
2022-11-15 21:31:42 +0100

    Added optional parameter to simple_poly for returning exact y

--- src/generate.py
+++ src/generate.py
@@ -1,40 +1,15 @@
 import numpy as np
 
-def simple_poly(n_points, coeffs = [3,0,1], noise_scale = 0.5):
+def simple_poly(n_points, coeffs = [3,0,1], noise_scale = 0.5, include_exact = False):
     """ Generates data for one variable polynomial """
 
     rng = np.random.default_rng()
     x = np.linspace([0],[2],n_points)
     n_vars = len(coeffs)
     noise = noise_scale*rng.normal(0,1,(n_points,1))
-    y = sum([coeffs[i]*x**i for i in range(n_vars)]) + noise
+    y_exact = sum([coeffs[i]*x**i for i in range(n_vars)])
+    y = y_exact + noise
 
-    return x,y
-
-def gen_simple(n_points, is_noiseless=False):
-    rng = np.random.default_rng()
-    """Generates simple test data"""
-    xn = np.linspace([0],[2],n_points)
-    yn = 4*xn+3 + rng.normal(0,1,(n_points,1))*0.5
-
-    x = np.arange(0,2,0.01)
-    y = 4*x+3
-
-    if is_noiseless:
-        return xn,yn,x,y
-    return xn, yn
-
-
-
-def gen_simple2(n_points, is_noiseless=False):
-    rng = np.random.default_rng()
-    """Generates simple test data"""
-    xn = np.linspace([0],[2],n_points)
-    yn = xn**2 + 3 + rng.normal(0,1,(n_points,1))*0.2
-
-    x = np.arange(0,2,0.01)
-    y = x**2 + 3
-
-    if is_noiseless:
-        return xn,yn,x,y
-    return xn, yn
+    if include_exact:
+        return x, y, y_exact
+    return x, y
